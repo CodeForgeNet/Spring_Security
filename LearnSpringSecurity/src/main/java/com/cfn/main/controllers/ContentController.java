a129c246f38920dbb9dff775ac3f3e8bfece8a95
CodeForgeNet <codeforgenet@icloud.com>
2025-08-16 02:13:03 +0530

    add proj

---
+++ LearnSpringSecurity/src/main/java/com/cfn/main/controllers/ContentController.java
@@ -0,0 +1,24 @@
+package com.cfn.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContentController {
+	
+	@GetMapping("/home")
+	public String handleWelcome() {
+		return "home";
+	}
+	
+	@GetMapping("/user/home")
+	public String handleUserHome() {
+		return "user";
+	}
+	
+	@GetMapping("/admin/home")
+	public String handleAdminHome() {
+		return "admin";
+	}
+	
+}
